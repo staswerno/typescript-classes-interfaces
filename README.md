afa23e714bd6174691673f6d1f836a3c15ab8b9d
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-03-29 16:57:29 +0200

    add Department class

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## classes and interfaces
 
 ### oop
+
+classes are blueprints for objects  
+objects are instances of classes  
+speeds up creation of objects  
+makes creation of multiple similar objects easier
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,49 @@
-// Code goes here!
+class Department {
+	name: string;
+	private employees: string[] = [];
+
+	constructor(n: string) {
+		this.name = n;
+	}
+
+	describe(this: Department) {
+		console.log("Department: " + this.name);
+	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Jopo");
+accounting.addEmployee("Jele");
+
+accounting.employees[2] = "Annu";
+// alternative method to add
+// we want to avoid this, have just one way
+// turn into private property/field (line 3)
+// can only be accessed from inside class
+
+console.log(accounting);
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { describe: accounting.describe };
+
+// accountingCopy.describe();
+// undefined, because not excecuting function on line 19.
+// passing function itself
+// so 'this' in method refers not to accounting object
+// rather, refers to thing responsible for calling method
+// ie accountingCopy, as called on accountingCopy
+// which is object has no name property, thus, undefined
+// TLDR: accounting object has name, accountingCopy doesn't
+// type safety added by  this: Department on line 8
